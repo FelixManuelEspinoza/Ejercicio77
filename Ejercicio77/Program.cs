8d403fbc9b5f09e61c866645c5b3e9553d065f57
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2022-10-19 17:26:35 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio77/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+
+class Ejercicio77
+{
+    static void Main()
+    {
+        SortedList<string,string> diccio = new SortedList<string,string>();
+        diccio.Add("Hello", "Hola");
+        diccio["see you later"] = "hasta luego";
+
+        Console.WriteLine(diccio["Hola"]);
+
+        if (diccio.ContainsKey("Adios"))
+            Console.WriteLine(diccio["Bye"]);
+    }
+}
